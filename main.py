c45d989c70bf95e5d3bde0e61c5aa10a64cd2696
Lóczy Levente <71691777+loczylevi@users.noreply.github.com>
2022-04-20 12:47:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+3. Európa legmagasabb épületei             (18pont)
+
+Az UTF-8 kódolású legmagasabb.txt állomány Európa legmagasabb épületeinek adatait
+tartalmazza a következő minta szerint (forrás: wikipedia.org):
+
+név; város; ország; magasság; emelet; épült
+10 Upper Bank Street; London; Anglia; 151; 32; 2003
+25 Bank Street; London; Anglia; 153; 33; 2003
+30 St Mary Axe; London; Anglia; 179, 8; 41; 2003
+...
+
+Az épület nevét, városát és országát az épület magassága (méter, valós szám), az emeletek
+száma és az épület építésének éve követi. Az adatokat pontosvessző választja el.
+
+3.1 Olvassa be az UTF-8 kódolású legmagasabb.txt állományban lévő adatokat és
+tárolja el egy saját osztály (Épület) típusú listában! Ügyeljen rá, hogy az állomány első
+sora az adatok fejlécét tartalmazza! A magasság érték tárolása (konvertálása) előtt cserélje
+le a szöveges típusú adatban a vesszőt pontra! Az osztály használata nem KÖTELEZŐ DE több pontot kaphat érte!!! 
+
+3.2 Határozza meg és írja ki a képernyőre, hogy hány épület található a forrásállományban!
+
+3.3 Határozza meg és írja ki a képernyőre az állományba található épületek emeleteinek az
+összegét!
+
+3.4 Határozza meg és írja ki a képernyőre a minta szerint, a legmagasabb épület adatait!
+Feltételezheti, hogy nem alakult ki holtverseny!
+
+3.5 Döntse el, hogy az adatok között található-e olasz épület! A keresését ne folytassa, ha a
+választ meg tudja adni!
+"""
